fa99eb11e06c6fa34b31f738834d0fa285e09122
AndrineElse <andrine.els@gmail.com>
2020-06-03 08:54:03 +0200

    Ready to test feature extraction in ipynb file

--- machine_learning/Dry_wet_cough_classification/clean_up.py
+++ machine_learning/Dry_wet_cough_classification/clean_up.py
@@ -9,13 +9,11 @@ Created on Wed Jun  3 07:05:49 2020
 import os
 from pydub import AudioSegment
 
-
+cwd = os.getcwd()
+data_dir = cwd + '/cough_data/'
 # Clean up the files in a dataset
 
 def rename_files():
-    cwd = os.getcwd()
-    data_dir = cwd + '/cough_data/'
-    
     for sub_dir in os.listdir(data_dir):
         i = 0
         for cough in os.listdir(data_dir + sub_dir):
@@ -43,4 +41,44 @@ def detect_silence(sound, silence_threshold=-50.0, chunk_size=10):
 
     return trim_ms
 
-os.makedirs(os.getcwd() + '/test')
+def trim(file):
+    sound = AudioSegment.from_file(file, format="wav")
+    
+    start_trim = detect_silence(sound)
+    end_trim = detect_silence(sound.reverse())
+    
+    duration = len(sound)    
+    trimmed_sound = sound[start_trim:duration-end_trim]
+    return trimmed_sound
+
+def trim_files():
+    for sub_dir in os.listdir(data_dir):
+        i = 0
+        for cough in os.listdir(data_dir + sub_dir):
+            path = (data_dir + sub_dir)
+            new_path  = (cwd + '/trimmed_cough_data/' + sub_dir)
+            new_name = new_path + '/' + 'trimmed_' + sub_dir+str(i)+'.wav'
+            i = i+1
+            test_file = trim(path + '/' + cough)
+            test_file.export(new_name, format="wav")
+
+
+def load_data():
+    dry = []
+    wet = []
+    data_path = cwd + '/trimmed_cough_data'
+    for sub_dir in os.listdir(data_path):
+        for cough in os.listdir(data_path + '/' +  sub_dir):
+            file_path = data_path + '/' +  sub_dir + '/' + cough
+            sound = AudioSegment.from_file(file_path, format="wav")
+            if sub_dir == 'dry':
+                dry.append(sound)
+            else:
+                wet.append(sound)
+    
+    return wet, dry
+
+
+
+wet, dry = load_data()
+
