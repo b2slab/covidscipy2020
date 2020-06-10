10d9bf9c14be6d3ac17a1bb07c12f009cda79b16
Chrischlord <64955750+Chrischlord@users.noreply.github.com>
2020-06-10 13:50:47 +0200

    Add files via upload
    a function that returns a specific datapoint from the database

---
+++ PythonApi/testGet_specificDataFunction.py
@@ -0,0 +1,23 @@
+import requests as req
+import json    # or `import simplejson as json` if on Python < 2.6
+
+def getSpecific(username, value): 
+    
+
+#Get all
+    url = 'http://127.0.0.1:5000/data/'+username+'?pretty'
+    resp = req.get(url)
+    jsondata=resp.text
+    obj = json.loads(jsondata) 
+    print(username, value, ":", obj[value])
+    
+    return obj[value]
+
+getSpecific("Christian","age")
+#print(resp.text) # Printing response
+
+#Get specific user (can perform url request with either
+# /data/<username> or /data/<_id>)
+#url = 'http://127.0.0.1:5000/data/TestUser130?pretty'
+#resp = req.get(url)
+#print(resp.text) # Printing response
