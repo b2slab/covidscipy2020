7f52035b836cf2813b06fc3d101c6c9362b09fd3
fl2399 <64913599+fl2399@users.noreply.github.com>
2020-06-13 10:41:10 +0200

    Update testGet_specificDataFunction.py
    added pseudocode for checking the validity of the user's input

--- PythonApi/testGet_specificDataFunction.py
+++ PythonApi/testGet_specificDataFunction.py
@@ -9,9 +9,20 @@ def getSpecific(username, values):
 #    print(username, values, ":", obj[values])
 #    return obj[values]
 
+    # Pseudocode 1: check if username exists
+    # if username doesn't exist:
+    #     print("No data for this username")
+    #     return None
+    
     specificValues = []
     print("Username: ", username)
     for element in values:
+        
+        # Pseudocode 2: check if element exists as a key in obj
+        # if element isn't part of obj:
+        #   print(element, " is not a valid key for this username")
+        #   pass
+        
         print(element, ": ", obj[element])
 #        specificValues.append(element)
         specificValues.append(obj[element])
