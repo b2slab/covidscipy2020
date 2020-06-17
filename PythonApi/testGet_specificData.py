d7d4f19541414037b09f84c5e44aaee9391d0c11
fl2399 <64913599+fl2399@users.noreply.github.com>
2020-06-17 12:28:36 +0200

    Update testGet_specificData.py
    Added user input to ask for specific user

--- PythonApi/testGet_specificData.py
+++ PythonApi/testGet_specificData.py
@@ -2,16 +2,22 @@ import requests as req
 import json    # or `import simplejson as json` if on Python < 2.6
 
 
-#Get all
-url = 'http://127.0.0.1:5000/data/TestUser112?pretty'
-resp = req.get(url)
-jsondata=resp.text
-obj = json.loads(jsondata) 
-print(obj["age"])
-#print(resp.text) # Printing response
+
+
+if __name__ == "__main__":
+  #Get all
+  user = input('Enter the username: ')
+
+  url = 'http://127.0.0.1:5000/data/' + user + '?pretty'
+  resp = req.get(url)
+  jsondata=resp.text
+  obj = json.loads(jsondata) 
+  #print(obj["age"])
+  #print(resp.text) # Printing response
 
 #Get specific user (can perform url request with either
 # /data/<username> or /data/<_id>)
 #url = 'http://127.0.0.1:5000/data/TestUser130?pretty'
 #resp = req.get(url)
 #print(resp.text) # Printing response
+
