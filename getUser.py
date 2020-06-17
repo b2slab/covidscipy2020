ae1e1ae0f5316eddecf6308246cc6a10b3cfbf9f
fl2399 <64913599+fl2399@users.noreply.github.com>
2020-06-17 12:47:31 +0200

    Create getUser.py
    new program to improve the request for the data entry of one user

---
+++ getUser.py
@@ -0,0 +1,18 @@
+import requests as req
+import json    # or `import simplejson as json` if on Python < 2.6
+
+def userEntry(username):
+  url = 'http://127.0.0.1:5000/data/' + user + '?pretty'
+  resp = req.get(url)
+  jsondata=resp.text
+  obj = json.loads(jsondata) 
+  print(obj) #for demonstration purposes
+  #print(resp.text) # Printing response
+  return obj
+  
+if __name__ == "__main__":
+  #Get all
+  user = input('Enter the username: ')  #for demonstration purposes
+  userEntry(user)   #for demonstration purposes, outside the program just call function userEntry and pass it the username
+
+ 
