4425a012d13beb2f08c5d33023c9e1a2c94c9d2d
guillembonilla <36695864+guillembonilla@users.noreply.github.com>
2020-11-07 11:52:21 +0100

    Catalan added as Language --- Spanish as default

--- Projecte COVID 2020-2021/Telegram Chatbot/mybot.py
+++ Projecte COVID 2020-2021/Telegram Chatbot/mybot.py
@@ -30,23 +30,22 @@ questions = {
     "en":{
         "q1":"Hi there! Please, enter your name.",
         "q2":"Cancelled",
-        "q3":"Allright we'll stop here\n"
-            "Please, give me a second while I upload the data.",
+        "q3":"Allright we'll stop here...\n\n Please, give me a second while I upload the data.",
         "q4":"That's it!",
         "q5":"Process stopped",
         "q6":"Username is invalid. Please enter a correct username.",
         "q7":"How old are you?",
-        "q8":"Age has to be a number.\nHow old are you? (digits only)",
-        "q9":"male",
-        "q10":"female",
-        "q11":"other",
+        "q8":"Age has to be a number.\n\n How old are you? (digits only)",
+        "q9":"Male",
+        "q10":"Female",
+        "q11":"Other",
         "q12":"Inadequate answer. Please choose one of the provided options",
         "q13":"Send Current Location",
         "q14":"Would you mind to send us your current location?\n\nPlease activate location on your device.",
         "q15":"Please activate location on your device and send it to us.",
-        "q16":"positive",
-        "q17":"negative",
-        "q18":"unknown",
+        "q16":"Positive",
+        "q17":"Negative",
+        "q18":"Unknown",
         "q19":"Do you have Covid-19?",
         "q20":"Could you send us a recording of your cough?",
         "q21":"Just use the audio message option from telegram and cough to the microphone.",
@@ -54,8 +53,8 @@ questions = {
         "q23":"Sorry, we didn't recognize this as cough. Please, cough again",
         "q24":"Thanks for your cough",
         "q25":"Do you have dry cough?",
-        "q26":"yes",
-        "q27":"no",
+        "q26":"Yes",
+        "q27":"No",
         "q28":"Do you have fever?",
         "q29":"Do you feel more tired than usual?",
         "q30":"Do you feel that you have lost/diminished your sense of smell?",
@@ -63,39 +62,39 @@ questions = {
         "q32":"Do you have chest pain or pressure?",
         "q33":"Do you have difficulty breathing or shortness of breath?",
         "q34":"Do you have any other information you would like to add?",
-        "q35":"Thank you very much for you collaboration!\n"
-            "Please, give me a second while I upload the data."
+        "q35":"Thank you very much for you collaboration!\n\n Please, give me a second while I upload the data.",
+
+        "q36":"What is your gender?"
 
     },
     "es":{
-        "q1":"Hola!, Por favor, introduzca su usuario.",
+        "q1":"Hola! Por favor, introduzca su nombre.",
         "q2":"Cancelado",
-        "q3":"Esta bien, lo dejamos aquí.\n"
-            "Deme un segundo mientras subo los datos.",
+        "q3":"Esta bien, lo dejamos aquí...\n\n Deme un segundo mientras almaceno los datos.",
         "q4":"Ya esta!",
         "q5":"Proceso detenido",
-        "q6":"Usuario invalido. Por favor introduzca un usuario correcto.",
-        "q7":"Cuantos años tiene?",
-        "q8":"Su edad debe ser un número.\nCuantos años tiene? (digitos)",
-        "q9":"hombre",
-        "q10":"mujer",
-        "q11":"otro",
-        "q12":"Respuesta inadecuada. Por favor elija entre las opciones del teclado",
-        "q13":"Envie su ubicación",
-        "q14":"Le importaria enviarnos su ubicación?\Por favor active la ubicación de su dispositivo",
+        "q6":"Usuario inválido. Por favor, introduzca un nombre correcto.",
+        "q7":"¿Cuántos años tiene?",
+        "q8":"Su edad debe ser un número.\n\n ¿Cuántos años tiene?",
+        "q9":"Hombre",
+        "q10":"Mujer",
+        "q11":"Otro",
+        "q12":"Respuesta incorrecta. Por favor, elija una de las opciones del teclado",
+        "q13":"Enviar su ubicación",
+        "q14":"¿Le importaría enviarnos su ubicación?\n\n Por favor, active la ubicación en su dispositivo.",
         "q15":"Por favor, active la ubicación en su dispositivo y envíenosla",
-        "q16":"positivo",
-        "q17":"negativo",
-        "q18":"desconocido",
+        "q16":"Positivo",
+        "q17":"Negativo",
+        "q18":"Desconocido",
         "q19":"¿Tiene usted covid-19?",
-        "q20":"¿Podria enviarnos una grabación de audio de su tos?",
-        "q21":"Basta con enviar una nota de audio de su tos.",
+        "q20":"¿Podría enviarnos una grabación de audio de su tos?",
+        "q21":"Por favor, envíenos una nota de voz de su tos.",
         "q22":"Analizando audio...",
-        "q23":"Lo sentimos, no hemos reconocido su audio como tos. ¿Podria volverlo a intentar?",
+        "q23":"Lo sentimos, no hemos reconocido su audio como tos. ¿Podría volverlo a intentar?",
         "q24":"Audio aceptado, gracias por su tos.",
         "q25":"¿Padece usted tos seca?",
-        "q26":"sí",
-        "q27":"no",
+        "q26":"Sí",
+        "q27":"No",
         "q28":"¿Padece usted fiebre?",
         "q29":"¿Se siente más cansado de lo normal?",
         "q30":"¿Siente un sentido del olfato reducido?",
@@ -103,9 +102,49 @@ questions = {
         "q32":"¿Padece dolor o presión en el pecho?",
         "q33":"¿Tiene dificultades para respirar?",
         "q34":"¿Hay algo más que quiera añadir?",
-        "q35":"¡Muchas gracias por su colaboración!\n"
-            "Aguarde un segundo mientras se guardan los datos."
+        "q35":"¡Muchas gracias por su colaboración!\n\n Aguarde un segundo mientras se guardan los datos.",
 
+        "q36":"¿Cuál es tu género?"
+
+    },
+    "ca":{
+        "q1":"Hola! Si us plau, introdueixi el seu nom.",
+        "q2":"Cancel·lat",
+        "q3":"Esta bé, ho deixem aquí...\n\n Doni'm un segon mentre emmagatzemo les dades.",
+        "q4":"Ja esta!",
+        "q5":"Procés detingut",
+        "q6":"Usuari invàlid. Si us plau, introdueixi un nom correcte.",
+        "q7":"Quants anys té?",
+        "q8":"La seva edat ha de ser un número. \n\n Quants anys té?",
+        "q9":"Home",
+        "q10":"Dona",
+        "q11":"Altre",
+        "q12":"Resposta incorrecta. Si us plau, triï una de les opcions de teclat",
+        "q13":"Enviar la seva ubicació",
+        "q14":"Li faria res enviar-nos la seva ubicació?\n\n Si us plau, activi la ubicació en el seu dispositiu.",
+        "q15":"Si us plau, activa la ubicació en el seu dispositiu i envieu-nos-la",
+        "q16":"Positiu",
+        "q17":"Negatiu",
+        "q18":"Desconegut",
+        "q19":"Té vostè covid-19?",
+        "q20":"Podria enviar-nos un enregistrament d'àudio de la seva tos?",
+        "q21":"Si us plau, envieu-nos una nota de veu de la seva tos.",
+        "q22":"Analitzant àudio ...",
+        "q23":"Ho sentim, no hem reconegut el seu àudio com a tos. Podria tornar-ho a intentar?",
+        "q24":"Audio acceptat, gràcies per la seva tos.",
+        "q25":"Pateix vostè tos seca?",
+        "q26":"Si",
+        "q27":"No",
+        "q28":"Pateix vostè febre?",
+        "q29":"¿Se sent més cansat del normal?",
+        "q30":"¿Sent un sentit de l'olfacte reduït?",
+        "q31":"Pateix mal de cap?",
+        "q32":"Pateix dolor o pressió al pit?",
+        "q33":"Té dificultats per respirar?",
+        "q34":"Hi ha alguna cosa més que vulgui afegir?",
+        "q35":"Moltes gràcies per la seva col·laboració! \n\n Esperi un segon mentre s'emmagatzemen les dades.",
+
+        "q36":"Quin és el teu gènere?"
     }
 }
 
@@ -194,10 +233,6 @@ DATABASE CONNECTED
 '''
 
 
-
-
-
-
 '''
 START CHATBOT
 '''
@@ -211,6 +246,11 @@ async def cmd_start(message: types.Message):
     global lang
     locale = message.from_user.locale
     lang = locale.language
+
+    # Si no reconoce idioma, por defecto activa el español
+    if lang not in ["en","es","ca"]:
+        lang = "es"
+    
     await Form.username.set()
     await message.reply(questions[lang]["q1"])
 
@@ -306,7 +346,7 @@ async def process_age(message: types.Message, state: FSMContext):
     markup.add(questions[lang]["q9"], questions[lang]["q10"])
     markup.add(questions[lang]["q11"])
 
-    await message.reply("What is your gender?", reply_markup=markup)
+    await message.reply(questions[lang]["q36"], reply_markup=markup)
 
 @dp.message_handler(lambda message: message.text not in [questions[lang]["q9"], questions[lang]["q10"], questions[lang]["q11"]], state=Form.gender)
 async def process_gender_invalid(message: types.Message):
@@ -451,7 +491,7 @@ async def process_cough(message: types.voice.Voice, state: FSMContext):
         return await message.reply(questions[lang]["q25"], reply_markup=markup)
 
     else:
-        return await bot.send_message(message.chat.id,message.chat.id,questions[lang]["q23"])
+        return await bot.send_message(message.chat.id,questions[lang]["q23"])
 
 
 
